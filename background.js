c7f9c2f784c7150ac6a3f5c47fe6d0406434abb8
xkym <xky1000@qq.com>
2022-09-05 14:17:52 +0800

    编程方式注入函数

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        files: ['content-script.js']
+        // files: ['content-script.js']
+        function: injectFunction
     });
 });
+
+function injectFunction() {
+    document.body.style.backgroundColor = 'green';
+}

ef4d6ba0c051ab90488d4c16b20b96ed2c654810
xkym <xky1000@qq.com>
2022-09-05 18:34:21 +0800

    信息传递

--- content-script.js
+++ content-script.js
@@ -1,1 +1,5 @@
-document.body.style.backgroundColor = 'orange';
+// document.body.style.backgroundColor = 'orange';
+
+chrome.runtime.sendMessage({ greeting: "hello" }, function (response) {
+    console.log(response.farewell);
+});
